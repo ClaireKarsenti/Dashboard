d602995d5d7f9a938b4fe3352364e361fce8d14e
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-12-11 19:16:25 +0100

    Add card elements

---
+++ app/ui/dashboard/card/card.tsx
@@ -0,0 +1,28 @@
+import { MdSupervisedUserCircle } from 'react-icons/md';
+import styles from './card.module.css';
+
+export type CardProps = {
+  title: string;
+  number: number;
+  change: number;
+};
+
+const Card = ({ title, number, change }: CardProps) => {
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>{title}</span>
+        <span className={styles.number}>{number}</span>
+        <span className={styles.detail}>
+          <span className={change > 0 ? styles.positive : styles.negative}>
+            {change}%
+          </span>{' '}
+          {change > 0 ? 'more' : 'less'} than previous week
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
