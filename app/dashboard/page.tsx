e353bdea131dbf392e49fd44c94c41809b910d46
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-12-11 20:26:14 +0100

    Add dashboard content to page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,22 @@
-export default function DashboardPage() {
-  return <div>DashboardPage</div>;
-}
+import Card from '../ui/dashboard/card/card';
+import Chart from '../ui/dashboard/chart/chart';
+import styles from '../ui/dashboard/dashboard.module.css';
+import RightBar from '../ui/dashboard/rightbar/rightbar';
+import Transactions from '../ui/dashboard/transactions/transactions';
+
+const Dashboard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <Card title={"Test"} number={70} change={30} />
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <RightBar />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
