1372ed356c16fdd798d3716a3c80b3769843662a
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-12-12 13:38:14 +0100

    Add users page

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,3 +1,17 @@
+import Search from '@/app/ui/dashboard/search/search';
+import styles from '@/app/ui/dashboard/users/users.module.css';
+import Link from 'next/link';
+
 export default function UsersPage() {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+        UsersPage
+      </div>
+    </div>
+  );
 }
