b9b35fe5ba753d4e5f85593c26d584c841fdc135
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-12-19 17:43:40 +0100

    Feat(products): fetch data

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,11 +1,28 @@
+import { fetchProducts } from '@/app/lib/data';
 import Pagination from '@/app/ui/dashboard/pagination/pagination';
-import Search from '@/app/ui/dashboard/search/search';
 import styles from '@/app/ui/dashboard/products/products.module.css';
+import Search from '@/app/ui/dashboard/search/search';
 import NoProduct from '@/public/noproduct.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
+import { ItemsProps } from '../users/page';
 
-export default function ProductsPage() {
+type Product = {
+  id: string;
+  img?: string;
+  title: string;
+  description: string;
+  price: number;
+  createdAt: string;
+  stock: number;
+  action: string;
+};
+
+export default async function ProductsPage({ searchParams }: ItemsProps) {
+  const query = searchParams?.query || '';
+  const page = searchParams?.page || 1;
+  const { products, count }: any = await fetchProducts({ query, page });
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -26,42 +43,47 @@ export default function ProductsPage() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src={NoProduct}
-                  alt="no product image"
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                MacBook
-              </div>
-            </td>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-              ipsa ex.
-            </td>
-            <td>$1999</td>
-            <td>13/12/2023</td>
-            <td>56</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/dashboard/products/testId">
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
-                  </button>
-                </Link>
-                <button className={`${styles.button} ${styles.delete}`}>
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
+          {products && products.length > 0 ? (
+            products?.map((product: Product) => (
+              <tr key={product.id}>
+                <td>
+                  <div className={styles.product}>
+                    <Image
+                      src={product.img || NoProduct}
+                      alt="product image"
+                      width={40}
+                      height={40}
+                      className={styles.productImage}
+                    />
+                    {product.title}
+                  </div>
+                </td>
+                <td>{product.description}</td>
+                <td>${product.price}</td>
+                <td>{product.createdAt?.toString().slice(4, 15)}</td>
+                <td>{product.stock}</td>
+                <td>
+                  <div className={styles.buttons}>
+                    <Link href={`/dashboard/users/${product.id}`}>
+                      <button className={`${styles.button} ${styles.view}`}>
+                        View
+                      </button>
+                    </Link>
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={6}>No products found</td>
+            </tr>
+          )}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
   );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -26,3 +26,23 @@ export const fetchUsers = async ({ query, page }: FetchItemsProps) => {
     throw new Error('❗️Failed to fetch users!');
   }
 };
+
+export const fetchProducts = async ({ query, page }: FetchItemsProps) => {
+  const regex = new RegExp(query, 'i');
+
+  const PRODUCT_PER_PAGE = 5;
+
+  try {
+    connectToDB();
+    const count = await Product.find({
+      title: { $regex: regex },
+    }).countDocuments();
+    const products = await Product.find({ title: { $regex: regex } })
+      .limit(PRODUCT_PER_PAGE)
+      .skip(PRODUCT_PER_PAGE * (page - 1));
+    return { count, products };
+  } catch (err) {
+    console.log(err);
+    throw new Error('❗️Failed to fetch products!');
+  }
+};
